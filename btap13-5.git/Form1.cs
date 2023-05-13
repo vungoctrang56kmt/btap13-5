f3c89e8f7a20cd073c4fe69ed0ad1ad411e838e2
nusyu <nusyu@TRANG>
2023-05-13 19:31:51 +0700

    Add project files.

---
+++ btap13-5.git/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace btap13_5.git
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo startsInfo = new ProcessStartInfo();
+            startsInfo.CreateNoWindow = true;
+            startsInfo.UseShellExecute = false;
+            startsInfo.FileName = "C:\\Program Files\\Java\\bin\\java.exe";
+            startsInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            startsInfo.RedirectStandardOutput = true;
+            startsInfo.WorkingDirectory = "D:\\kiemtrath2";
+            startsInfo.Arguments = $"ngoctrang {textBox1.Text} {textBox2.Text}";
+            try
+            {
+                using (Process exe = Process.Start(startsInfo))
+                {
+                    exe.WaitForExit();
+                    while (!exe.StandardOutput.EndOfStream)
+                    {
+                        string line = exe.StandardOutput.ReadLine();
+                        textBox3.AppendText(line + "\n");
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                textBox3.AppendText(ex.Message);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
